a534987ed386caf859b5f8b6b86e5d5ff8328578
jokerety <avatar51@mail.ru>
2018-11-14 16:38:16 +0300

    fixed

--- DigitalSignage_create.sql
+++ DigitalSignage_create.sql
@@ -1,33 +1,28 @@
 USE `DigitalSignage`;
 
-drop table `payments`;
-drop table `contracts`;
+drop table `orders`;
 drop table `companies`;
+drop table `contracts`;
+drop table `payments`;
+drop table `priorities`;
 drop table `tariffs`;
 
 
-CREATE TABLE companies (
-	company_id INT NOT NULL AUTO_INCREMENT,
-    phone VARCHAR(45) NOT NULL,
-    reg_dttm TIMESTAMP,
-    PRIMARY KEY(company_id)
-) ;
 CREATE TABLE tariffs (
     tariff_id INT NOT NULL ,
     tariff_name VARCHAR(45) NOT NULL,
-    monthly_fee DOUBLE NOT NULL,
     PRIMARY KEY (tariff_id)
 );
-
+CREATE TABLE priorities (
+	priority_id INT NOT NULL AUTO_INCREMENT,
+    message VARCHAR (45) NOT NULL,
+    PRIMARY KEY (priority_id)
+) ;
 CREATE TABLE payments (
 	payment_id INT NOT NULL AUTO_INCREMENT,
-    company_id INT NOT NULL,
-    discount INT NOT NULL,
-    tariff_id INT NOT NULL,
+    amount DOUBLE,
     payment_dttm TIMESTAMP,
-    PRIMARY KEY(payment_id),
-    FOREIGN KEY (company_id) REFERENCES companies(company_id),
-    FOREIGN KEY (tariff_id) REFERENCES tariffs(tariff_id)
+    PRIMARY KEY(payment_id)
 ) ;
 
 CREATE TABLE contracts (
@@ -35,6 +30,39 @@ CREATE TABLE contracts (
 	company_id INT NOT NULL,
     begin_dttm TIMESTAMP,
     end_dttm TIMESTAMP,
-    PRIMARY KEY(contract_id),
-    FOREIGN KEY (company_id) REFERENCES companies(company_id)
+    PRIMARY KEY(contract_id)
 ) ; 
+
+CREATE TABLE companies (
+	company_id INT NOT NULL AUTO_INCREMENT,
+    phone VARCHAR(45) NOT NULL,
+    name VARCHAR(45) NOT NULL,
+    joined_dttm TIMESTAMP,
+    balance  DOUBLE,
+    contract_id INT NOT NULL,
+    PRIMARY KEY(company_id),
+    FOREIGN KEY (contract_id) REFERENCES contracts(contract_id),
+    FOREIGN KEY (company_id) REFERENCES payments (payment_id)
+) ;
+CREATE TABLE orders (
+	order_id INT NOT NULL AUTO_INCREMENT,
+    description_order VARCHAR (250) NOT NULL,
+	discount INT NOT NULL,
+    order_dttm TIMESTAMP,
+    company_id INT NOT NULL,
+    tariff_id INT NOT NULL,
+    priority_id INT NOT NULL,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (company_id) REFERENCES companies(company_id),
+    FOREIGN KEY (tariff_id) REFERENCES tariffs(tariff_id),
+    FOREIGN KEY (priority_id) REFERENCES priorities(priority_id)
+) ;
+
+
+
+
+
+
+
+
+
